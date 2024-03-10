84c4ba7185f01d91dc8dd6cc0abe982550a12385
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-03-10 23:22:54 +0900

    init commit

---
+++ src/main/java/com/sparta/myselectshop/MyselelctshopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselelctshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyselelctshopApplication.class, args);
+	}
+
+}
