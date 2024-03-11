74ec9d4a91de86a0e8adef92c969b5f33d4904c8
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-03-11 15:32:19 +0900

    폴더별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -110,4 +110,16 @@ public class ProductService {
         productFolderRepository.save(new ProductFolder(product, folder));
         // 데이터베이스 로우 한 줄이 객체 하나이다.
     }
+
+    public Page<ProductResponseDto> getProductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc, User user) {
+
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<Product> productList = productRepository.findAllByUserAndProductFolderList_FolderId(user, folderId, pageable);
+
+        Page<ProductResponseDto> responseDtoList = productList.map(ProductResponseDto::new);
+        return responseDtoList;
+    }
 }
