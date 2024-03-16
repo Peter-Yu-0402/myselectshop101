9c4426c88ceb559b2c88cfbee65b887cfd812b6f
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-03-16 20:34:42 +0900

    [FEAT] Swagger 초기 설정

--- src/main/java/com/sparta/myselectshop/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/myselectshop/config/WebSecurityConfig.java
@@ -60,11 +60,22 @@ public class WebSecurityConfig {
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
 
+        String[] PUBLIC_URL = {"/v1/api-docs",
+                "/swagger-resources/**",
+                "/api/v1/**",
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/v1/api-docs/**",
+                "/api-docs/**",
+                "api-docs" };
+
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
                         .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers(PUBLIC_URL).permitAll() // swagger 관련 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
--- src/main/java/com/sparta/myselectshop/controller/FolderController.java
+++ src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -4,19 +4,23 @@ import com.sparta.myselectshop.dto.FolderRequestDto;
 import com.sparta.myselectshop.dto.FolderResponseDto;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.FolderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Folder API", description = "폴더와 관련된 API 정보를 담고 있습니다.")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class FolderController {
 
     private final FolderService folderService;
 
+    @Operation(summary = "폴더 등록 기능", description = "폴더를 등록할 수 있는 API")
     @PostMapping("/folders")
     public void addFolders(@RequestBody FolderRequestDto folderRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         List<String> foldernames = folderRequestDto.getFolderNames();
@@ -24,6 +28,7 @@ public class FolderController {
 
     }
 
+    @Operation(summary = "폴더 조회 기능", description = "폴더을 조회할 수 있는 API")
     @GetMapping("/folders")
     public List<FolderResponseDto> getFolders(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return folderService.getFolders(userDetails.getUser());
